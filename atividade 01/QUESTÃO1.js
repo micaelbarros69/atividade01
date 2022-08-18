ec59ed21d63ce287ecf1fe8eeecc1a7d1e8387dc
Micael Barros <102255563+micaelbarros69@users.noreply.github.com>
2022-08-18 16:28:42 -0300

    Add files via upload

---
+++ atividade 01/QUESTÃO1.js
@@ -0,0 +1,10 @@
+var raio = prompt("Raio do circulo:");
+raio = parseFloat(raio)
+ var comprimento = 2* Math.PI * raio;
+ var area = 2 * Math.PI * raio * raio;
+ 
+ document.write ("O Comprimento = ", comprimento); 
+ document.write ("A Area = ", area);
+
+
+
